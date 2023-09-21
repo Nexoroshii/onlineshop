1614898922ac102c86f831817e711e875a9fe677
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-09-21 22:52:59 +0300

    add pages and logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import styles from '../../styles/Header.module.css';
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '../../utils/routes';
+import LOGO from '../../images/logo.svg';
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <div className={styles.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt="logo"></img>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
