9ea8d2c635b6432d58af40c7df3d2bc92c8e586b
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-09-22 00:11:35 +0300

    start footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import styles from '../../styles/Footer.module.css';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../utils/routes';
+import LOGO from '../../images/logo.svg';
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <section className={styles.footer}>
+            <div className={styles.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt="logo"></img>
+                </Link>
+            </div>
+        </section>
+    );
 };
 
 export default Footer;
