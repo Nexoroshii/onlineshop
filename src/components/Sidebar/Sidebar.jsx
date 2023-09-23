48db5d0f545a67b0668afd5fbe7cce89792746d7
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-09-23 19:27:43 +0300

    finish sideBar and create categoriesSlice

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
 import styles from '../../styles/Sidebar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
-    return <div>Sidebar</div>;
+    return (
+        <section>
+            <div className={styles.sidebar}>
+                <div className={styles.title}>CATEGORIES</div>
+                <nav>
+                    <ul className={styles.menu}>
+                        <li>
+                            <NavLink to={`/categories/${1}`}>Link</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className={styles.footer}>
+                    <a href="/help" target="_blank" className={styles.link}>
+                        Help
+                    </a>
+                    <a
+                        href="/terms"
+                        target="_blank"
+                        className={styles.link}
+                        style={{ textDecoration: 'underline' }}
+                    >
+                        Terms & Conditions
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Sidebar;
