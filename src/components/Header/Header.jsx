ae76d2d1d70d62438ab2cd3cd59a011d9f043f20
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-09-27 23:51:31 +0300

    finish register method

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,78 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-import styles from "../../styles/Header.module.css";
-import { Link } from "react-router-dom";
+import styles from '../../styles/Header.module.css';
+import { Link } from 'react-router-dom';
 
-import { ROUTES } from "../../utils/routes";
-import LOGO from "../../images/logo.svg";
-import AVATAR from "../../images/avatar.jpg";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleForm } from "../../features/user/userSlice";
+import { ROUTES } from '../../utils/routes';
+import LOGO from '../../images/logo.svg';
+import AVATAR from '../../images/avatar.jpg';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleForm } from '../../features/user/userSlice';
 
 const Header = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const { currentUser } = useSelector(({ user }) => user);
+    const { currentUser } = useSelector(({ user }) => user);
 
-  const handleClick = () => {
-    if (!currentUser) {
-      dispatch(toggleForm(true));
-    }
-  };
-  return (
-    <div className={styles.header}>
-      <div className={styles.logo}>
-        <Link to={ROUTES.HOME}>
-          <img src={LOGO} alt="logo"></img>
-        </Link>
-      </div>
+    const [values, setValues] = useState({ name: 'Guest', avatar: AVATAR });
 
-      <div className={styles.info}>
-        <div className={styles.user} onClick={handleClick}>
-          <div
-            className={styles.avatar}
-            style={{ backgroundImage: `url(${AVATAR})` }}
-          />
-          <div className={styles.username}>Guest</div>
+    useEffect(() => {
+        if (!currentUser) return;
+
+        setValues(currentUser);
+    }, [currentUser]);
+
+    const handleClick = () => {
+        if (!currentUser) {
+            dispatch(toggleForm(true));
+        }
+    };
+    return (
+        <div className={styles.header}>
+            <div className={styles.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt="logo"></img>
+                </Link>
+            </div>
+
+            <div className={styles.info}>
+                <div className={styles.user} onClick={handleClick}>
+                    <div
+                        className={styles.avatar}
+                        style={{ backgroundImage: `url(${values.avatar})` }}
+                    />
+                    <div className={styles.username}>{values.name}</div>
+                </div>
+
+                <form className={styles.form}>
+                    <div className={styles.icon}>
+                        <svg className="icon">
+                            <use
+                                xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`}
+                            />
+                        </svg>
+                    </div>
+
+                    <div className={styles.input}>
+                        <input
+                            type="search"
+                            name="search"
+                            placeholder="Search for anything..."
+                            autoComplete="off"
+                            onChange={() => {}}
+                            value=""
+                        />
+                    </div>
+                    {false && <div className={styles.box}></div>}
+                </form>
+
+                <div className={styles.account}>
+                    <Link to={ROUTES.HOME} className={styles.favourites}>
+                        <svg className={styles['icon-fav']}>
+                            <use
+                                xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`}
+                            />
+                        </svg>
+                    </Link>
+
+                    <Link to={ROUTES.CART} className={styles.cart}>
+                        <svg className={styles['icon-cart']}>
+                            <use
+                                xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`}
+                            />
+                        </svg>
+                        <span className={styles.count}>2</span>
+                    </Link>
+                </div>
+            </div>
         </div>
-
-        <form className={styles.form}>
-          <div className={styles.icon}>
-            <svg className="icon">
-              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`} />
-            </svg>
-          </div>
-
-          <div className={styles.input}>
-            <input
-              type="search"
-              name="search"
-              placeholder="Search for anything..."
-              autoComplete="off"
-              onChange={() => {}}
-              value=""
-            />
-          </div>
-          {false && <div className={styles.box}></div>}
-        </form>
-
-        <div className={styles.account}>
-          <Link to={ROUTES.HOME} className={styles.favourites}>
-            <svg className={styles["icon-fav"]}>
-              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`} />
-            </svg>
-          </Link>
-
-          <Link to={ROUTES.CART} className={styles.cart}>
-            <svg className={styles["icon-cart"]}>
-              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`} />
-            </svg>
-            <span className={styles.count}>2</span>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
--- src/components/User/UserSignupForm.jsx
+++ src/components/User/UserSignupForm.jsx
@@ -1,82 +1,95 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import styles from "../../styles/User.module.css";
+import styles from '../../styles/User.module.css';
+import { useDispatch } from 'react-redux';
+import { createUser } from '../../features/user/userSlice';
 
 const UserSignupForm = ({ closeForm }) => {
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    password: "",
-    avatar: "",
-  });
+    const dispatch = useDispatch();
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        avatar: '',
+    });
 
-  const handleChange = ({ target: { value, name } }) => {
-    setValues({ ...values, [name]: value });
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    const handleChange = ({ target: { value, name } }) => {
+        setValues({ ...values, [name]: value });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isEmpty = Object.values(values).some((val) => !val);
+        if (isEmpty) return;
+        dispatch(createUser(values));
+        closeForm();
+    };
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.close} onClick={closeForm}>
-        <svg className="icon">
-          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#close`} />
-        </svg>
-      </div>
-      <div className={styles.title}>SignUp</div>
-      <form className={styles.form}>
-        <div className={styles.group}>
-          <input
-            type="email"
-            placeholder="Your email"
-            name="email"
-            value={values.email}
-            autoComplete="off"
-            onChange={handleChange}
-            required
-          />
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.close} onClick={closeForm}>
+                <svg className="icon">
+                    <use
+                        xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#close`}
+                    />
+                </svg>
+            </div>
+            <div className={styles.title}>SignUp</div>
+            <form className={styles.form}>
+                <div className={styles.group}>
+                    <input
+                        type="email"
+                        placeholder="Your email"
+                        name="email"
+                        value={values.email}
+                        autoComplete="off"
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className={styles.group}>
+                    <input
+                        type="name"
+                        placeholder="Your name"
+                        name="name"
+                        value={values.name}
+                        autoComplete="off"
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className={styles.group}>
+                    <input
+                        type="password"
+                        placeholder="Your password"
+                        name="password"
+                        value={values.password}
+                        autoComplete="off"
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className={styles.group}>
+                    <input
+                        type="avatar"
+                        placeholder="Your avatar"
+                        name="avatar"
+                        value={values.avatar}
+                        autoComplete="off"
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className={styles.link}> I already have an account </div>
+                <button
+                    type="submit"
+                    className={styles.submit}
+                    onClick={handleSubmit}
+                >
+                    Create an account
+                </button>
+            </form>
         </div>
-        <div className={styles.group}>
-          <input
-            type="name"
-            placeholder="Your name"
-            name="name"
-            value={values.name}
-            autoComplete="off"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.group}>
-          <input
-            type="password"
-            placeholder="Your password"
-            name="password"
-            value={values.password}
-            autoComplete="off"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.group}>
-          <input
-            type="avatar"
-            placeholder="Your avatar"
-            name="email"
-            value={values.avatar}
-            autoComplete="off"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.link}> I already have an account </div>
-        <button type="submit" className={styles.submit} onClick={handleSubmit}>
-          Create an account
-        </button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default UserSignupForm;
