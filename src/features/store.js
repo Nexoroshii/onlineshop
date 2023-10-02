e4ceb6ea91267cc158bd5c68364b01fadaad9bde
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-10-02 15:15:45 +0300

    little fixes

--- src/features/store.js
+++ src/features/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import categoriesSlice from './categories/categoriesSlice';
 import productsSlice from './products/productsSlice';
 import { apiSlice } from './api/apiSlice';
